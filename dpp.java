7f58466022e632837fafa0cb3960a14d58e99923
Karen Grace Lee <kglee@unix.lt.ucsc.edu>
2018-01-20 12:16:28 -0800

    scan lines in both text files and put into an array

--- dpp.java
+++ dpp.java
@@ -15,20 +15,21 @@ class dpp{
         Scanner in2 = new Scanner(new File(args[1]));
         PrintWriter out = new PrintWriter(new FileWriter("transformations.txt"));
 
-        // Count number of lines and store each line in String[]
-        in = new Scanner(new File(args[0]));
-        String line = in.nextLine();
-        int count = 0;
-        while (line != null) {
-            count++;
-            line.nextLine();
-        }
-        in.close();
-
         ArrayList text1 = new ArrayList();
         ArrayList text2 = new ArrayList();
-        for (int i = 0; i < count; i++) {
         
+        // Scan 2nd text file first and insert into an array
+        String line = in2.nextLine();
+        while (line != null) {
+        	text2.add(line);
+        	line = in2.nextLine();
+        }
+        
+        // Scan 1st text file and insert into an array
+        line = in1.nextLine();
+        while (line != null) {
+        	text1.add(line);
+        	line = in1.nextLine();
         }
 
         // Close files
