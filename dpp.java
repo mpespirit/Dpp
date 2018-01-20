e0764236626c5dec8f0566903fd44f91b397d828
mpespirit <mespirit@ucsc.edu>
2018-01-20 00:45:42 -0800

    modified starter code

--- dpp.java
+++ dpp.java
@@ -1,21 +1,18 @@
-//Dpp
+//dpp
 
-import java.io.*;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
-class Dpp{    
+class dpp{    
     public static void main(String[] args) throws IOException{
-        Scanner in = null;
-        PrintWriter out = null;
-        //String line = "";
-        
         if(args.length != 2){
-            System.err.println("Usage: FileIO infile outfile");
+            System.err.println("Usage: dpp text1 text2");
             System.exit(1);
         }
 
-        in = new Scanner(new File(args[0]));
-        out = new PrintWriter(new FileWriter(args[1]));
+        Scanner in1 = new Scanner(new File(args[0]));
+        Scanner in2 = new Scanner(new File(args[1]));
+        PrintWriter out = new PrintWriter(new FileWriter("transformations.txt"));
 
         // Count number of lines and store each line in String[]
         in = new Scanner(new File(args[0]));
