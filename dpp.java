f0611aaacc0fff3bc8e57c515af93d19df216e31
mpespirit <mespirit@ucsc.edu>
2018-01-20 18:57:43 -0800

    Getting input file read properly. Moving on to 2D array lists and writing transformation functions

--- dpp.java
+++ dpp.java
@@ -1,40 +1,51 @@
-//dpp
+// dpp - a better diff tool
+// This program takes in an input file and scrambles it based on a 
+// provided sequence transformations. Possible transformations include
+// add, delete, and transpose. 
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class dpp{    
-    public static void main(String[] args) throws IOException{
-        if(args.length != 2){
-            System.err.println("Usage: dpp text1 text2");
-            System.exit(1);
-        }
+class dpp{   
+  void add(){
+  
+  }
 
-        Scanner in1 = new Scanner(new File(args[0]));
-        Scanner in2 = new Scanner(new File(args[1]));
-        PrintWriter out = new PrintWriter(new FileWriter("transformations.txt"));
+  void delete() {
 
-        ArrayList text1 = new ArrayList();
-        ArrayList text2 = new ArrayList();
-        
-        // Scan 2nd text file first and insert into an array
-        String line = in2.nextLine();
-        while (line != null) {
-        	text2.add(line);
-        	line = in2.nextLine();
-        }
-        
-        // Scan 1st text file and insert into an array
-        line = in1.nextLine();
-        while (line != null) {
-        	text1.add(line);
-        	line = in1.nextLine();
-        }
+  }
 
-        // Close files
-        in1.close();
-        in2.close();
-        out.close();
+  //swap method
+  void transpose(){
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    if(args.length != 1){
+      System.err.println("Usage: dpp text1 text2");
+      System.exit(1);
     }
+
+    Scanner in = new Scanner(new File(args[0]));
+    PrintWriter out = new PrintWriter(new FileWriter("out"));
+
+    ArrayList<String> text = new ArrayList<String>();
+        
+    // Scan 1st text file and insert into an array
+    while(in.hasNextLine()){
+      text.add(in.nextLine());
+    }
+
+    for(int i = 0; i < text.size(); i++){
+      out.println(text.get(i));
+    }   
+
+    // Close files
+    in.close();
+    out.close();
+  }
 }
